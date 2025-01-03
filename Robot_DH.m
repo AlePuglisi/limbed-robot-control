54ea4aa7a20d6bc3da9601bc2950c2c529b7a8cb
Alessandro Puglisi <puglisialessandro27@gmail.com>
2025-01-03 13:15:08 +0100

    contact 4-limb model

--- Robot_DH.m
+++ Robot_DH.m
@@ -35,7 +35,7 @@ ROBOT = [LF_leg, LH_leg, RH_leg, RF_leg];
 q0 = zeros(1,N_link);
 q = q0; 
 
-% Plote the robot 
+% Plot the robot 
 figure('Name', 'LIMBERO+GRIEEL q=qz')
 hold on
 for i = 1:N_limb
@@ -59,3 +59,30 @@ hold off
 title('Robot zero configuration, Swing limbs');
 
 %% CONTACT LEGS MODEL %% 
+g = [0; 0; 9.81]; % Gravity vector
+q0_contact = zeros(1,N_link);
+% Homogeneus Transformations for Base to Limbroot position
+tx_ee = limbero_contact.fkine(q0_contact).t(1);
+t = tx_ee*sqrt(2)/2;
+T_LF = transl(L/2+t, W/2+t, 0)*trotz(-(pi/2+pi/4)*180/pi)*limbero_contact.base.T;
+T_LH =  transl(-L/2-t, W/2+t, 0)*trotz(-(pi/4)*180/pi)*limbero_contact.base.T;
+T_RH =  transl(-L/2-t, -W/2-t, 0)*trotz((pi/4)*180/pi)*limbero_contact.base.T;
+T_RF =  transl(L/2+t, -W/2-t, 0)*trotz((pi/4+pi/2)*180/pi)*limbero_contact.base.T;
+% Leg redefinition
+LF_leg_contact = SerialLink(limbero_contact, 'name', 'LF_{contact}', 'gravity', g, 'base', T_LF);
+LH_leg_contact = SerialLink(limbero_contact, 'name', 'LH_{contact}', 'gravity', g, 'base', T_LH);
+RH_leg_contact = SerialLink(limbero_contact, 'name', 'RH_{contact}', 'gravity', g, 'base', T_RH);
+RF_leg_contact = SerialLink(limbero_contact, 'name', 'RF_{contact}', 'gravity', g, 'base', T_RF);
+
+ROBOT_CONTACT = [LF_leg_contact, LH_leg_contact, RH_leg_contact, RF_leg_contact]; 
+
+q0_contact = zeros(1,N_link);
+q_contact = q0_contact;
+
+% Plote the robot 
+figure('Name', 'LIMBERO+GRIEEL CONTACT q=qz')
+hold on
+for i = 1:N_limb
+    ROBOT_CONTACT(i).plot(q_contact, 'nobase', 'noshadow', 'notiles'); 
+end
+
